c899f0351eacb870bb424a7e966f1ac13c2059ec
Alim Mamutov <mamutov.creation@yandex.ru>
2021-12-03 12:11:20 +0300

    Разобрался с механизмом валидации моделей Описал в README.md

--- authapp/models.py
+++ authapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
+from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 # Create your models here.
 
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.core.validators import RegexValidator
-from django.core.exceptions import ValidationError
 # Create your models here.
 
 onlyLatin = RegexValidator(
