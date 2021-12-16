ec19d0828241ef2541992a521e33dc0f9e16aada
Alim Mamutov <mamutov.creation@yandex.ru>
2021-12-16 16:09:21 +0300

    Добавил ссылку на администрирование в навигационное меню

--- adminapp/views.py
+++ adminapp/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 
 # Create your views here.
 from authapp.models import ShopUser
 
 
+@user_passes_test(lambda x: x.is_superuser) #  Этот декоратор пропустит в админку только юзера, удовл условиям в скобках
 def index(request):
 
     users_list = ShopUser.objects.all().order_by('-is_active', '-is_superuser', '-is_staff', 'username')
