9d981b8c57196d37e120bc597898ef26aa45961d
Alim Mamutov <mamutov.creation@yandex.ru>
2021-12-16 16:52:42 +0300

    Добавил создание пользователя, редактирование пользователя, удаление пользователя

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 
 import adminapp.views as adminapp  # Здесь импортируем наш контроллер из views
 
-app_name = 'adminapp' # Указываю, что этот файл работает в пространстве имен adminapp
+app_name = 'adminapp'  # Указываю, что этот файл работает в пространстве имен adminapp
 
 urlpatterns = [
     path('', adminapp.index, name='index'),
+    path('user/create/', adminapp.user_create, name='user_create'),
+    path('user/update/<int:pk>/', adminapp.user_update, name='user_update'),
+    path('user/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
 ]
